46ce1eb121af7e391f72d1a80d5d2ac12a5c7090
autocanibal <mondzzmkhize@gmail.com>
2023-07-29 16:56:49 +0200

    Initial commit

---
+++ src/main/java/org/mondemkhize/jd522fa1/JD522FA1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.mondemkhize.jd522fa1;
+
+/**
+ *
+ * @author Monde
+ */
+public class JD522FA1 {
+    public static void main(String[] args) {
+        loginForm log = new loginForm();
+        log.setVisible(true);
+    }
+}
